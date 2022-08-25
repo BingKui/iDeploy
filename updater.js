73eb0b3f231c1676d510f976a089b243ad1fbdaf
康兵奎 <178974662@qq.com>
2022-08-25 20:05:46 +0800

    update

--- updater.js
+++ updater.js
@@ -14,11 +14,13 @@ const getLinuxName = (name) => {
     return result;
 }
 
-const content = fs.readFileSync('./package.json', { encoding: 'utf-8' });
+const content = fs.readFileSync('./package.json', {
+    encoding: 'utf-8'
+});
 const packageInfo = JSON.parse(content);
 const versionContent = `{
     "version": "v${packageInfo.version}",
-    "notes": "1.更新版本测试\n2.测试多行更新",
+    "notes": "解决了一些已知问题。",
     "pub_date": "${(new Date()).toISOString()}",
     "platforms": {
         "darwin-x86_64": {
@@ -28,19 +30,18 @@ const versionContent = `{
         "darwin-aarch64": {
             "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHJzaWduIGVuY3J5cHRlZCBzZWNyZXQga2V5ClJXUlRZMEl5a1JIbXBSWko2aE5jOG4yTjg4U0wrNE9lWVVNR3Y1ZGpSUFVuOHFWNmdOWUFBQkFBQUFBQUFBQUFBQUlBQUFBQW1TblhQZ3lqa1dsRUlJWGdEVTJwU01qZHRXKzh5dE9xK3AyMmpSbjVrK1RqYmtPemlGZHNDQkl6K09yOXRKUVBSSWN2emlnMjdwQkNPbFcvbEQ2bmprdFZOM0RxQy9zWXkzb3B0UVlRbG9NUEhzSFNPQ0xHcllpcHBuT29jb0hacUVjaTY1QVBwNkk9Cg==",
             "url": "https://oss.uiseed.cn/app/${packageInfo.name}/arm/${packageInfo.name}.app.tar.gz"
-        },
-        "linux-x86_64": {
-            "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHJzaWduIGVuY3J5cHRlZCBzZWNyZXQga2V5ClJXUlRZMEl5a1JIbXBSWko2aE5jOG4yTjg4U0wrNE9lWVVNR3Y1ZGpSUFVuOHFWNmdOWUFBQkFBQUFBQUFBQUFBQUlBQUFBQW1TblhQZ3lqa1dsRUlJWGdEVTJwU01qZHRXKzh5dE9xK3AyMmpSbjVrK1RqYmtPemlGZHNDQkl6K09yOXRKUVBSSWN2emlnMjdwQkNPbFcvbEQ2bmprdFZOM0RxQy9zWXkzb3B0UVlRbG9NUEhzSFNPQ0xHcllpcHBuT29jb0hacUVjaTY1QVBwNkk9Cg==",
-            "url": "https://oss.uiseed.cn/app/${packageInfo.name}/linux/${getLinuxName(packageInfo.name)}_${packageInfo.version}_amd64.AppImage.tar.gz"
-        },
-        "windows-x86_64": {
-            "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHJzaWduIGVuY3J5cHRlZCBzZWNyZXQga2V5ClJXUlRZMEl5a1JIbXBSWko2aE5jOG4yTjg4U0wrNE9lWVVNR3Y1ZGpSUFVuOHFWNmdOWUFBQkFBQUFBQUFBQUFBQUlBQUFBQW1TblhQZ3lqa1dsRUlJWGdEVTJwU01qZHRXKzh5dE9xK3AyMmpSbjVrK1RqYmtPemlGZHNDQkl6K09yOXRKUVBSSWN2emlnMjdwQkNPbFcvbEQ2bmprdFZOM0RxQy9zWXkzb3B0UVlRbG9NUEhzSFNPQ0xHcllpcHBuT29jb0hacUVjaTY1QVBwNkk9Cg==",
-            "url": "https://oss.uiseed.cn/app/${packageInfo.name}/windows/${packageInfo.name}_${packageInfo.version}_x64_en-US.msi.zip"
         }
     }
 }`;
 
-fs.writeFileSync('./latest.json', versionContent, { encoding: 'utf-8' });
-
-// console.log(content);
-// console.log(versionContent);
+// "linux-x86_64": {
+//     "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHJzaWduIGVuY3J5cHRlZCBzZWNyZXQga2V5ClJXUlRZMEl5a1JIbXBSWko2aE5jOG4yTjg4U0wrNE9lWVVNR3Y1ZGpSUFVuOHFWNmdOWUFBQkFBQUFBQUFBQUFBQUlBQUFBQW1TblhQZ3lqa1dsRUlJWGdEVTJwU01qZHRXKzh5dE9xK3AyMmpSbjVrK1RqYmtPemlGZHNDQkl6K09yOXRKUVBSSWN2emlnMjdwQkNPbFcvbEQ2bmprdFZOM0RxQy9zWXkzb3B0UVlRbG9NUEhzSFNPQ0xHcllpcHBuT29jb0hacUVjaTY1QVBwNkk9Cg==",
+//     "url": "https://oss.uiseed.cn/app/${packageInfo.name}/linux/${getLinuxName(packageInfo.name)}_${packageInfo.version}_amd64.AppImage.tar.gz"
+// },
+// "windows-x86_64": {
+//     "signature": "dW50cnVzdGVkIGNvbW1lbnQ6IHJzaWduIGVuY3J5cHRlZCBzZWNyZXQga2V5ClJXUlRZMEl5a1JIbXBSWko2aE5jOG4yTjg4U0wrNE9lWVVNR3Y1ZGpSUFVuOHFWNmdOWUFBQkFBQUFBQUFBQUFBQUlBQUFBQW1TblhQZ3lqa1dsRUlJWGdEVTJwU01qZHRXKzh5dE9xK3AyMmpSbjVrK1RqYmtPemlGZHNDQkl6K09yOXRKUVBSSWN2emlnMjdwQkNPbFcvbEQ2bmprdFZOM0RxQy9zWXkzb3B0UVlRbG9NUEhzSFNPQ0xHcllpcHBuT29jb0hacUVjaTY1QVBwNkk9Cg==",
+//     "url": "https://oss.uiseed.cn/app/${packageInfo.name}/windows/${packageInfo.name}_${packageInfo.version}_x64_en-US.msi.zip"
+// }
+fs.writeFileSync('./latest.json', versionContent, {
+    encoding: 'utf-8'
+});
